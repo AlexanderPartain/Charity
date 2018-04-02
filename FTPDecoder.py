399e0fd6cc6f84ec932263641833db74d2f9056e
Kimmie <katienza67@gmail.com>
2018-04-01 21:17:28 -0500

    Update FTPDecoder.py

--- FTPDecoder.py
+++ FTPDecoder.py
@@ -1,10 +1,29 @@
 from ftplib import FTP
-import sys
+import sys, re
 
-ftp = FTP('jeangourd.com')
+#* means needs a number value
+
+def decode(x,n):
+    #split binary string into n-sized bits
+	binary = [x[i : i + n] for i in range(0, len(x),n)]
+
+    #covert to ASCII and print
+	for ch in binary:
+		print(chr(int(ch,2)), end="")
+
+ftp = FTP('jeangourd.com') #could also do FTP(host)
+#ftp.connection(host, int(port, *port#))
+#if user = "":
 ftp.login("anonymous","");
+#else:
+#ftp.login(user, password)
 data = []
  
 ftp.dir()
- 
+
+#takes dir listing 
+binary_String = ""
+#needs to get dir list
+#binary_String = 
+#decode(binary_String, int(mode,10)) 
 ftp.quit()
